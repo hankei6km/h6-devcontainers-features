c95caab92cc4ede06fa1fbbe5ee164cd4ad14151
hankei6km <gh-hankei6km@outlook.jp>
2023-12-17 06:55:46 +0000

    chore: Add bump-ver.sh script for version bumping
    Upon further consideration(#9), it is necessary to bump the version
    number, so I added a script.

---
+++ scripts/bump-ver.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+# exmaple:
+# scripts/ver-tag.sh [feature-name] patch
+# scripts/ver-tag.sh [feature-name] prelease pre-.
+
+FEATURE="${1}"
+RANGE="${2}"
+PREREL="${3}"
+test -n "${FEATURE}" || { echo "Missing feature name"; exit 1; }
+
+LATEST="$(jq -r '.version' "src/${FEATURE}/devcontainer-feature.json")"
+
+VERSION="$(
+    if [ "${RANGE}" = "prelease" ]; then
+        semver bump "${RANGE}" "${PREREL}}" "${LATEST}"
+    else
+        semver bump "${RANGE}" "${LATEST}"
+    fi
+)"
+echo "New version: ${VERSION}"
+
+# Update devcontainer-feature.json
+jq ".version = \"${VERSION}\"" "src/${FEATURE}/devcontainer-feature.json" > "src/${FEATURE}/devcontainer-feature.json.tmp"
+mv "src/${FEATURE}/devcontainer-feature.json.tmp" "src/${FEATURE}/devcontainer-feature.json"
+
+git add "src/${FEATURE}/devcontainer-feature.json"
+MESSAGE="feature_${FEATURE}_${VERSION}"
+git commit -m "${MESSAGE}"
+
+echo "Operations for release:"
+echo "- git push origin"
+echo "- Run the release workflow on GitHub"
