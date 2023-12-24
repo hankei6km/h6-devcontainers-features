353bd2d7714621e77aafd921747ee9bbe11c3e15
hankei6km <gh-hankei6km@outlook.jp>
2023-12-24 11:06:34 +0000

    feat: Add "semver-tool" feature

---
+++ test/semver-tool/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+# Provides the 'check' and 'reportResults' commands.
+# shellcheck source=/dev/null
+source dev-container-features-test-lib
+
+# Check for installed features
+check "Check semver-tool is installed" test -x /usr/local/bin/semver
+check "Check execution of semver-tool command" test "$(semver bump minor 1.0.3)" = "1.1.0"
+
+# Report result
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
